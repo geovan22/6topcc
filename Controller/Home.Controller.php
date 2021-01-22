a5e92832ac32add02d321997cb51e048e4b7eb0c
geovan22 <geovan221315@hotmail.com>
2021-01-22 11:32:58 -0600

    index
    controlador frontal

---
+++ Controller/Home.Controller.php
@@ -0,0 +1,15 @@
+<?php
+    class Home
+    {
+        public function Inicio()
+        {
+            echo "estas en el controlador home y metodo inicio"; 
+            
+        }
+        
+        public function Fin()
+        {
+            echo "estas en el controlador home y metodo fin";    
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
+
+    include('Controller/Home.controller.php');
+
    if(isset($_GET['controller']) && isset($_GET['method']))
    {
-        echo "existen las variables";
+        $controller=$_GET['controller'];
+        $method=$_GET['method'];
    }
    else
    {
-        echo "no variables";
+        $controller="Home";
+        $method="Inicio";
    }
+   
+   if(class_exists($controller) && method_exists($controller,$method))
+   {
+        $class=new $controller;
+        $class->$method();
+   }
+   else
+   {
+       echo "clase no existe o el metodo es incorrecto";
+   }
+ 
 ?>
